29d6a4fc3d2eb2c2a4e1869deeb63176ea4aef1d
C_hristian Lisantti <114838310+Incmplir@users.noreply.github.com>
2023-03-30 20:15:59 -0300

    info traducida

--- src/layout/Info/Info.jsx
+++ src/layout/Info/Info.jsx
@@ -4,22 +4,21 @@ import React from "react";
 const Info = () => {
     return (
         <>
-            <Section title="Yo" idSection='yo'>
-                <div className='flex flex-row gap-2 items-center'>
+            <Section title="Yo" idSection="yo">
+                <div className="flex flex-row gap-2 items-center">
                     <div class="container-logo-1 bg-text-100/80">
                         <div class="container-logo-2 bg-primary-100/80">
                             <div class="container-logo-3 w-20 h-20"></div>
                         </div>
                     </div>
-
-                <p class="text">
-                    A few years ago, I became interested in developing websites
-                    and interfaces. Since then, I have been steadily improving
-                    my skills. Now I am developing websites and web applications
-                    to order or for myself.
-                </p>
+                    <p class="text">
+                        Hace un tiempo, me interesé en el desarrollo web e
+                        interfaces. Desde entonces, he ido mejorando
+                        constantemente mis habilidades. Ahora estoy
+                        desarrollando sitios web y aplicaciones web por encargo
+                        o por diversión.
+                    </p>
                 </div>
-
             </Section>
         </>
     );
